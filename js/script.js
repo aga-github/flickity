7b5f1611a28d58a758eb9ae9bfe4f2b8fa1ff904
Aga Matyjasik <agus.wroclaw@gmail.com>
2019-06-10 16:30:58 +0200

    Add html structure, scss style and js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var elem = document.querySelector(".carousel");
+var flkty = new Flickity(elem, {
+    hash: true,
+    cellAlign: "left",
+    contain: true,
+    pageDots: false,
+});
+
+var flkty = new Flickity('.carousel');
+
+// Ktoś miał też:
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
+});
